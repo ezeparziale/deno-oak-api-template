6492dfbd8f2f5808aeb83333773ec855152c249b
Ezequiel Parziale <eparziale@gmail.com>
2023-05-29 17:19:27 -0300

    :wrench: update: add db env vars

--- src/configs/general.config.ts
+++ src/configs/general.config.ts
@@ -2,4 +2,12 @@ import { load } from '../deps.ts'
 
 const env = await load()
 
+// Server
 export const PORT: number = Number(env['PORT']) || 3000
+
+// Postgres Database
+export const POSTGRES_HOSTNAME: string = env['POSTGRES_HOSTNAME']
+export const POSTGRES_PORT: number = Number(env['POSTGRES_PORT']) || 5432
+export const POSTGRES_PASSWORD: string = env['POSTGRES_PASSWORD']
+export const POSTGRES_USER: string = env['POSTGRES_USER']
+export const POSTGRES_DB: string = env['POSTGRES_DB']
