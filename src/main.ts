9047a46e56b843321f445c19a02c65bfb92c0836
Ezequiel Parziale <eparziale@gmail.com>
2023-05-29 12:20:08 -0300

    :rocket: update: import user router

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,13 @@
-import { Application, Router } from './deps.ts'
-
-const router = new Router()
-router.get('/', (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `
-})
+import { Application } from './deps.ts'
+import { PORT } from './configs/general.config.ts'
+import router from './routes/user.router.ts'
 
 const app = new Application()
+
+// Routes
 app.use(router.routes())
+
 app.use(router.allowedMethods())
 
-const PORT = 3000
 console.log(`Server running on port: ${PORT}`)
 app.listen({ port: PORT })
