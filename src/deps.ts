364458f22d1af40928ee04c5c317821a48d6771b
Ezequiel Parziale <eparziale@gmail.com>
2023-05-29 17:20:54 -0300

    :pushpin: update: add new deps
    - denodb
    - oak

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,15 @@
-export { Application, Response, Router } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
+export {
+  Application,
+  Context,
+  helpers,
+  Request,
+  Response,
+  Router,
+} from 'https://deno.land/x/oak@v12.5.0/mod.ts'
 export { load } from 'https://deno.land/std/dotenv/mod.ts'
+export {
+  Database,
+  DataTypes,
+  Model,
+  PostgresConnector,
+} from 'https://deno.land/x/denodb/mod.ts'
