faf7314d4f42db459f461be5d56a37202e608cdc
Ezequiel Parziale <eparziale@gmail.com>
2023-05-29 17:22:54 -0300

    :rocket: update: add init db to main app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Application } from './deps.ts'
 import { PORT } from './configs/general.config.ts'
 import router from './routes/user.router.ts'
+import initDb from './db/init.db.ts'
 
 const app = new Application()
 
@@ -8,6 +9,9 @@ const app = new Application()
 app.use(router.routes())
 
 app.use(router.allowedMethods())
+
+// Database
+await initDb()
 
 console.log(`Server running on port: ${PORT}`)
 app.listen({ port: PORT })
