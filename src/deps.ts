30c6f4548b2132140600206f0d90f794d05b3559
Ezequiel Parziale <eparziale@gmail.com>
2023-05-29 12:19:01 -0300

    :pushpin: update: add new deps and dotenv

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,2 @@
-export { Application, Router } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
+export { Application, Response, Router } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
+export { load } from 'https://deno.land/std/dotenv/mod.ts'
