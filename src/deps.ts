12ceeef9afc16c01e48054005b19b3cb49cd9ffa
Ezequiel Parziale <eparziale@gmail.com>
2023-05-30 19:41:58 -0300

    :pushpin: update: set specific version

--- src/deps.ts
+++ src/deps.ts
@@ -6,11 +6,11 @@ export {
   Response,
   Router,
 } from 'https://deno.land/x/oak@v12.5.0/mod.ts'
-export { load } from 'https://deno.land/std/dotenv/mod.ts'
+export { load } from 'https://deno.land/std@0.190.0/dotenv/mod.ts'
 export {
   Database,
   DataTypes,
   Model,
   PostgresConnector,
-} from 'https://deno.land/x/denodb/mod.ts'
+} from 'https://deno.land/x/denodb@v1.4.0/mod.ts'
 export * as bcrypt from 'https://deno.land/x/bcrypt@v0.4.1/mod.ts'
